42b0a197570c576b739b7a0a82329e0b1c96d075
Ken Dresdell <kdresdell@gmail.com>
2014-10-30 19:19:43 -0400

    Code pour Backup

--- bin/backup-wp.sh
+++ bin/backup-wp.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
  
-# Backup MySQL database and compress with max niceness
-
-mysqldump -u wp_admin -p wordpress | nice -n 19 gzip -f > wp-db-backup.sql.gz
-
 DBNAME="wordpress"
 DBPASSWORD="wpadmin&2014"
 DBUSER="wp_admin"
-
+BK_PATH="/root/backups/"
 
 DATE=`date +"%Y%m%d"`
-SQLFILE=$DBNAME-${DATE}.sql
-
-mysqldump -u $DBUSER -p\'wpadmin&2014' $DBNAME > $SQLFILE
-#gzip $SQLFILE
+SQLFILE=$BK_PATH$DBNAME-${DATE}.sql
+WEBFILE=$BK_PATH'WEBFILES'-${DATE}.tar.gz
 
 
+##
+## ADD THE FOLOWING CONJOB
+##
+
+# 0 2 * * * /root/jmdstore/bin/backup-wp.sh > /dev/null 2>&1
+
+
+
+
+
+##
+## BACKUP THE DATABASE
+##
+
+mysqldump -u $DBUSER -p'wpadmin&2014' $DBNAME > $SQLFILE
+gzip $SQLFILE
+
+
+##
+## BACKUP WEB FILES
+##
 
 #tar czvf wp-backup.tgz /usr/share/nginx/html/
+tar cvf - /usr/share/nginx/html/ --exclude='*TMP_IMG*' | gzip -9 - > $WEBFILE
 
-#echo "File backup done"
 

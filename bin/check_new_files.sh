a1dfdc9e72af23e5bb6692ce78be02c1c32f5c0e
Ken Dresdell <kdresdell@gmail.com>
2014-11-03 08:14:35 -0500

    Correction du script pour alarme si noouveau CSV

--- bin/check_new_files.sh
+++ bin/check_new_files.sh
@@ -7,7 +7,7 @@
 #*/30 * * * * /root/jmdstore/bin/check_new_files.sh > /dev/null 2>&1
 
 
-if [ `find /home/kdresdell/test/ -name "*.c2sv"` ]
+if [ `find /home/ftpsyncuser/ -name "*.csv"` ]
 
 then
   echo "Nouveau fichier CSV JMD" | mail -s "Nouveau CSV JMD" kdresdell@gmail.com 

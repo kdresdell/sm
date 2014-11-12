092a4e0ec47842fca347aa93a4d0504c8f5e30df
Ken Dresdell <kdresdell@gmail.com>
2014-11-11 20:24:46 -0500

    Correction de bug

--- bin/ScanFiles.sh
+++ bin/ScanFiles.sh
@@ -12,8 +12,6 @@ MAIL_USER="kdresdell@gmail.com"
 DONE_PATH="/home/GoogleDrive/"
 U_WEB_FILE="/usr/share/nginx/html/CSV_QUEUE/U_WEB.csv"
 
-
-
 TT=$(date -d "today" +"%Y%m%d%H%M")
 
 
@@ -21,30 +19,27 @@ for f in $FILES
 do
   if [ -f $f ]; then
     echo "Nouveau fichier CSV de JMD" | mail -s "Nouveau CSV JMD" $MAIL_USER
-    echo "There is a WEB.CSV file in FTPSYNCUSER FOLDER............"
-    
-    echo "CONVERTING RICHARD'S CSV FILE FOR WPALLIMPORT SPEC......"
+
+    # CONVERTING JMD'S CSV FILE FOR WPALLIMPORT SPEC
     #/root/jmdstore/bin/CONV_CSV.py  $f
 
     # VALIDE SI LE FICHIER DE CONVERSION EST PRESENT
     if [ -f $U_WEB_FILE ]; then
-      echo "There is U_WEB_FILE..................................."
-      echo "Starting the import trigger..........................."
-      #curl --insecure "https://www.sportsjmd.com/wp-cron.php?import_key=LhZ0aEx5tfq&import_id=18&action=trigger" &
-      sleep 20&
+      #echo "There is U_WEB_FILE..................................."
+      #echo "Starting the import trigger..........................."
+      curl --insecure "https://www.sportsjmd.com/wp-cron.php?import_key=LhZ0aEx5tfq&import_id=18&action=trigger" &
       triggerPID=$!
-      echo "triggerPID is $triggerPID"
-      echo "Waiting for import......."
+      #echo "triggerPID is $triggerPID"
+      #echo "Waiting for import......."
       while test -d /proc/$triggerPID; do
-        echo "Curl processing aux 2 minutes durant l'importation......"
-        #curl --insecure "https://www.sportsjmd.com/wp-cron.php?import_key=LhZ0aEx5tfq&import_id=18&action=processing"
-        #sleep 120
-        sleep 1
+        #echo "Curl processing aux 2 minutes durant l'importation......"
+        curl --insecure "https://www.sportsjmd.com/wp-cron.php?import_key=LhZ0aEx5tfq&import_id=18&action=processing"
+        sleep 120
       done
 
       # JE RENOMME LES FICHIERS AVEC TIME STAMP POUR GARDER UN HISTORIQUE
       mv $f $DONE_PATH/WEB_$TT.CSV
-      mv $U_WEB_FILE $DONE_PATH/U_WEB_$TT
+      mv $U_WEB_FILE $DONE_PATH/U_WEB_$TT.csv
 
     fi
   fi
